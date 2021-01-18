72966ddd863621c3fa1e19f971620b88ebd904e6
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-01-18 16:50:57 +0530

    1 st solve

---
+++ 50 questions/smalleronleft.cpp
@@ -0,0 +1,71 @@
+/* code by kartikeya sharma */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* predefined value */
+#define yes() cout << "YES" 
+#define no() cout << "NO"
+#define one() cout << "1"
+#define two() cout << "0"
+#define ll long long int
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+
+/* loops */
+#define inp(i,n,arr) for(int i=0 ; i<n ; i++) cin >> arr[i]
+#define out(i,n,arr) for(int i=0 ; i<n ; i++) cout << arr[i] << " "
+#define loop(i,n) for(int i=0;i<n;i++)
+
+/* typedef */
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+/* code */
+void solve()
+{
+
+    
+    int n;
+    cin >> n;
+    int arr[n];
+    inp(i,n,arr);
+
+  set<int> s;
+    set<int>::iterator it;
+
+    for (int i = 0; i < n; i++) {
+         
+        s.insert(arr[i]);
+         
+      
+        it = s.lower_bound(arr[i]);
+ 
+        if (it == s.begin()) {
+            cout << "-1"
+                << " ";
+        }
+        else {
+            it--;
+            cout << (*it) << " ";
+        }
+    }           
+    
+    
+	
+}
+
+int main() {
+	
+	int t;
+	cin>> t;
+	while(t--)
+	{
+	    
+	   solve();
+       cout<<endl;
+	}
+
+	return 0;
+}
