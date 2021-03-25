b5dbf281ad036a3d736141e07560d141abedd227
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-03-25 20:03:39 +0530

    lets do div 3

---
+++ 2021 kick/cde.cpp
@@ -0,0 +1,44 @@
+/* code by savagecarol */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* predefined value */
+#define yes() cout << "YES" 
+#define no() cout << "NO"
+#define one() cout << "1"
+#define zero() cout << "0"
+#define ll long long int
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+
+/* loops */
+#define inp(i,n,arr) for(int i=0 ; i<n ; i++) cin >> arr[i]
+#define out(i,n,arr) for(int i=0 ; i<n ; i++) cout << arr[i] << " "
+#define loop(i,n) for(int i=0;i<n;i++)
+
+/* typedef */
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+void solve(ll i)
+{
+
+
+
+
+
+}
+
+int main() {
+    ll t;
+    cin >> t;
+    for(ll i =1 ; i <=t; i++)
+         {
+            solve(i);
+            cout << endl;
+        }
+    return 0 ;
+}
+
