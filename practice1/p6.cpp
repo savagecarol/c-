7295b4d701b2c065891e2ea3ba597c834fd707d6
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-04-18 23:51:55 +0530

    d;lfgk

---
+++ practice1/p6.cpp
@@ -0,0 +1,106 @@
+/* code by savagecarol */
+#include <bits/stdc++.h>
+using namespace std;
+
+/* predefined value */
+#define yes() cout << "YES" 
+#define no() cout << "NO"
+#define one() cout << "1"
+#define two() cout << "0"
+#define ll long long int
+#define intt long long
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+
+/* loops */
+#define inp(i,n,arr) for(int i=0 ; i<n ; i++) cin >> arr[i]
+#define out(i,n,arr) for(int i=0 ; i<n ; i++) cout << arr[i] << " "
+#define loop(i,n) for(int i=0;i<n;i++)
+
+#define mod 1000000007
+ ll dp[2000][2000];
+
+
+/* typedef */
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+/* code */ 
+void solve()
+{
+    ll n ;
+    cin >> n;
+    ll q;
+    cin >> q;
+    ll a[n];
+    inp(i , n , a);
+    ll x , y;
+
+
+    ll dp[n*(n+1)/2];
+    ll k = 0;
+    for(ll i = 0 ; i < n ;i++)
+    {
+        dp[k] = a[i];
+        k++;
+        ll d = a[i];
+        for(ll j = i+1 ; j < n ;j++)
+        {
+            d=d & a[j];
+            dp[k] = d;
+            k++;
+        }
+    }
+
+    ll d = 0;
+    for(ll i = 0 ; i < n*(n+1)/2 ;i++)
+    {
+         d =d | dp[i];
+    }
+    cout << d << endl;
+
+    loop(i , q)
+    {
+
+    cin >> x >> y;
+    a[x-1] = y;
+    ll k = 0;
+    for(ll i = 0 ; i < n ;i++)
+    {
+        dp[k] = a[i];
+        k++;
+        ll d = a[i];
+        for(ll j = i+1 ; j < n ;j++)
+        {
+            d=d & a[j];
+            dp[k] = d;
+            k++;
+        }
+    }
+
+    ll d = 0;
+    for(ll i = 0 ; i < n*(n+1)/2 ;i++)
+    {
+         d =d | dp[i];
+    }
+    cout << d << endl;
+    
+    }
+}
+
+
+
+
+int main()
+{
+    ll t;
+    cin >> t;
+
+    while(t--)
+    {
+	solve();
+    }
+	return 0;
+}
--- practice1/tempCodeRunnerFile.cpp
+++ practice1/tempCodeRunnerFile.cpp
@@ -30,24 +30,37 @@ typedef pair<ll, ll> pll;
 /* code */ 
 void solve()
 {
-    ll x1 , y1 , x2 , y2;
-    cin >> x1 >> y1 >> x2 >> y2;
-        if (x1 != x2 && y1 != y2 && abs(x1 - x2) != abs(y1 - y2))
-        cout << -1 << endl;
-        else if (x1 == x2)
-        cout << x1 + abs(y1 - y2) << " " << y1 << " " << x2 + abs(y1 - y2) << " " << y2 << endl;
-        else if (y1 == y2)
-        cout << x1 << " " << y1 + abs(x1 - x2) << " " << x2 << " " << y2 + abs(x1 - x2) << endl;
-        else
-        cout << x1 << " " << y2 << " " << x2 << " " << y1 << endl;
+
+    ll n , k;
+    cin >> n >> k;
+    ll a[n];
+    inp(i , n , a );
+    unordered_map<ll , ll> r;
+
+    loop(i , n)
+    {
+        r[a[i]]++;
+    }
+
+    ll p = n - r.size();
+    if(p>=k)
+    {
+        cout << r.size();
+    }
+    else
+    {
+        cout << r.size() - (k- p);
+    }
+
 }
 int main()
 {
     ll t;
-    t = 1;
+    cin >> t;
     while(t--)
     {
 	solve();
+    cout << endl;
     }
 	return 0;
 }
