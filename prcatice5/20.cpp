33bf08a6f7578a519166c1452ed6c36a4f1ec52b
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-05-25 16:40:19 +0530

    dsgfdfg

---
+++ prcatice5/20.cpp
@@ -0,0 +1,93 @@
+/* code by savagecarol */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/* predefined value */
+#define yes() cout << "YES" 
+#define no() cout << "NO"
+#define one() cout << "1"
+#define zero() cout << "0"
+#define ll long long int
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+ 
+/* loops */
+#define inp(i,n,arr) for(ll i=0 ; i<n ; i++) cin >> arr[i]
+#define out(i,n,arr) for(ll i=0 ; i<n ; i++) cout << arr[i] << " "
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define arrs(i,n,arr,sum) for(ll i = 0 ; i < n ; i++) sum = sum + arr[i];
+#define arrmax(i , n , arr , maxx) for(ll i = 1 ; i < n;i++) maxx = max(maxx , arr[i]);
+#define arrmin(i , n , arr , minn) for(ll i = 0 ; i < n;i++) minn = min(minn , arr[i]); 
+ 
+ 
+/* typedef */
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+/* power function */
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+int gcd (int a, int b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+int countSetBits(int n){
+    unsigned int count = 0;
+    while (n) {
+        n &= (n - 1);
+        count++;
+        }
+    return count;
+}
+
+ int powerOfTwo(int n)
+    {
+        return n && (!(n & (n-1)));
+    }
+/*code*/
+
+void solve() 
+{ int N;
+    cin>>N;
+    int A[N],i;
+    for(i=0;i<N;i++){
+        cin>>A[i];
+    }
+    sort(A,A+N);
+    int ans=1,pos=0,curr=INT_MAX;
+    for(i=1;i<N;i++){
+        curr = min(curr,abs(A[i]-A[i-1]));
+        if(curr<A[i]){
+            ans = i;
+            break;
+        }
+        ans++;
+    }
+    cout<<ans;
+   
+}
+int main() 
+{
+
+	ll t;
+    cin >> t;
+	while(t--)
+	{
+	   solve();
+       cout << endl;
+	}
+	return 0;
+}
