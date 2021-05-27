99eef51557998620b4f37b0a25ed6137839c5062
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-05-27 16:47:39 +0530

    enefkf

---
+++ prcatice5/33.cpp
@@ -0,0 +1,105 @@
+/* code by savagecarol */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/* predefined value */
+#define yes() cout << "YES" 
+#define no() cout << "NO"
+#define one() cout << "1"
+#define zero() cout << "0"
+#define ll long long int
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+ 
+/* loops */
+#define inp(i,n,arr) for(ll i=0 ; i<n ; i++) cin >> arr[i]
+#define out(i,n,arr) for(ll i=0 ; i<n ; i++) cout << arr[i] << " "
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define arrs(i,n,arr,sum) for(ll i = 0 ; i < n ; i++) sum = sum + arr[i];
+#define arrmax(i , n , arr , maxx) for(ll i = 1 ; i < n;i++) maxx = max(maxx , arr[i]);
+#define arrmin(i , n , arr , minn) for(ll i = 0 ; i < n;i++) minn = min(minn , arr[i]); 
+ 
+ 
+/* typedef */
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+/* power function */
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+int gcd (int a, int b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+int countSetBits(int n){
+    unsigned int count = 0;
+    while (n) {
+        n &= (n - 1);
+        count++;
+        }
+    return count;
+}
+
+ int powerOfTwo(int n)
+    {
+        return n && (!(n & (n-1)));
+    }
+/*code*/
+
+void solve() 
+{
+    ll n , k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    k=k+1;
+    ll count = 0;
+    for(ll i = 0 ; i <n;)
+    {
+        if(s[i] == '0')
+        {
+            ll flag = true;
+            ll j = i+1;
+            for( j = i+1; j <min(i+k, n);j++)
+            {
+                if(s[j] == '1')
+                {
+                flag = false ;
+                break;
+                }
+            }
+            if(flag == true)count++;
+            i=j;
+        }
+        else
+        {
+            i=i+k;
+        }
+    }
+    cout << count;
+
+}
+int main() 
+{
+	ll t;
+    cin >> t;
+	while(t--)
+	{
+	   solve();
+       cout << endl;
+	}
+	return 0;
+}
