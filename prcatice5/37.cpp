b40b2316937fc4305adb528b06b900118e51fe54
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-05-27 19:34:51 +0530

    dsfgf

---
+++ prcatice5/37.cpp
@@ -0,0 +1,102 @@
+/* code by savagecarol */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/* predefined value */
+#define yes() cout << "YES" 
+#define no() cout << "NO"
+#define one() cout << "1"
+#define zero() cout << "0"
+#define ll long long int
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+ 
+/* loops */
+#define inp(i,n,arr) for(ll i=0 ; i<n ; i++) cin >> arr[i]
+#define out(i,n,arr) for(ll i=0 ; i<n ; i++) cout << arr[i] << " "
+#define loop(i,n) for(ll i=0;i<n;i++)
+#define arrs(i,n,arr,sum) for(ll i = 0 ; i < n ; i++) sum = sum + arr[i];
+#define arrmax(i , n , arr , maxx) for(ll i = 1 ; i < n;i++) maxx = max(maxx , arr[i]);
+#define arrmin(i , n , arr , minn) for(ll i = 0 ; i < n;i++) minn = min(minn , arr[i]); 
+ 
+ 
+/* typedef */
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+/* power function */
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+int gcd (int a, int b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+int countSetBits(int n){
+    unsigned int count = 0;
+    while (n) {
+        n &= (n - 1);
+        count++;
+        }
+    return count;
+}
+
+ int powerOfTwo(int n)
+    {
+        return n && (!(n & (n-1)));
+    }
+/*code*/
+
+void solve() 
+{
+   string s;
+   cin >> s;
+   vector<string> a;
+   ll n = s.length();
+   if(n==1){cout << -1 ; return;}   
+   ll it = -1;
+   for(ll i = 0 ; i < s.length()-1 ; i++)
+   {
+       if(s[i] == '2' || s[i] == '4' || s[i]=='6' || s[i] == '8' || s[i] == '0')
+       {
+           if(s[i] < s[n-1])
+           {
+            swap(s[i] , s[n-1]);
+            cout << s;
+            return;
+           }
+           else
+           {
+               it = i;
+           }
+       }
+   }
+   if(it==-1)cout <<-1;
+   else{
+       swap(s[it] , s[n-1]);
+       cout << s;
+   }
+}
+int main() 
+{
+	ll t;
+    t=1;
+	while(t--)
+	{
+	   solve();
+       cout << endl;
+	}
+	return 0;
+}
